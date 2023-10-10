8bf04e10eb0d66fdad1af19463f7fb4f8306f43f
Andrea-Canale <andrea.canale@istitutoagnelli.it>
2023-10-10 10:51:02 +0200

    chore: Bump dependencies

--- website/svelte.config.js
+++ website/svelte.config.js
@@ -2,21 +2,17 @@
 
 import adapter from '@sveltejs/adapter-static';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte',
 		adapter: adapter({
 			// default options are shown
 			pages: 'build',
 			assets: 'build',
 			fallback: null
 		}),
-		router: true,
 		prerender: {
 			crawl: true,
-			enabled: true,
-			onError: 'fail',
 			entries: ['*']
 		},
 	}
